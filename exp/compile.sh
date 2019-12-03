844fb16ae572ba1b543cb8b90d6140ffec40871e
msserpa <matheusserpa@gmail.com>
2019-12-03 09:28:20 -0300

    change flags

---
+++ exp/compile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail -o posix
+
+mkdir -p bin/
+cd ../
+
+for version in der1der1hm der1der1lm der1der1 original; do
+	cd $version
+	for backend in OpenMP CUDA OpenACC; do	
+		make backend=$backend
+		mv ModelagemFletcher.exe ../exp/bin/$version.$backend.`hostname`.x
+		make clean backend=$backend
+	done
+	cd ..
+done
---
+++ exp/env.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Choose gcc or icc
+
+# gcc
+# export GCC=gcc
+# export OMP_FLAG=-fopenmp
+
+# icc 
+source /home/intel/bin/iccvars.sh intel64
+
+export GCC=icc
+export OMP_FLAG=-qopenmp
+
+# pgcc
+export PGI=/home/pgi
+export LD_LIBRARY_PATH=/home/pgi/linux86-64/19.10/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export MANPATH=$MANPATH:$PGI/linux86-64/19.10/man
+export LM_LICENSE_FILE=$PGI/license.dat
+export PATH=$PGI/linux86-64/19.10/bin:$PATH
+
+export PGCC_GPU_SM=cc60 # change GPU capability
+
+# cuda
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib
+
+export CUDA_GPU_SM=sm_60 # change GPU capability
